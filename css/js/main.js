01308c95e71973ab3a37a6b11b7a8e9dfa49e004
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-02-07 13:47:19 +0700

    Delete css/js/main.js

--- css/js/main.js
+++
@@ -1,9 +0,0 @@
-function openWhatsApp(text) {
-  const msg = encodeURIComponent(
-    "Hello! I am interested in: " + text
-  );
-  window.open(
-    "https://wa.me/84778532223?text=" + msg,
-    "_blank"
-  );
-}
